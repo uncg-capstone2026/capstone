fd563d2bd3dda4730f40de90ffb60e4459a1aa1e
Taylor Edwards <WTEDWARDS@uncg.edu>
2026-09-26 10:06:16 -0400

    Server setup: config module, prisma module, /api prefix, CORS, PORT

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { S3Module } from './s3/s3.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [S3Module],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    S3Module,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
